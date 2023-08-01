820b5daa79253ec31744de1bedf1eb13fbea3544
UsmanTahirKiani <49678080+UsmanTahirKiani@users.noreply.github.com>
2023-08-01 14:32:44 +0500

    Update config.py
    Change in Config file, for default setting

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 
 class BaseConfig(object):
-    CACHE_TYPE = os.environ.get('CACHE_TYPE')
-    CACHE_REDIS_HOST = os.environ.get('CACHE_REDIS_HOST')
-    CACHE_REDIS_PORT = os.environ.get('CACHE_REDIS_PORT')
-    CACHE_REDIS_DB = os.environ.get('CACHE_REDIS_DB')
+    CACHE_TYPE = os.environ.get('CACHE_TYPE', 'redis')
+    CACHE_REDIS_HOST = os.environ.get('CACHE_REDIS_HOST', 'localhost')
+    CACHE_REDIS_PORT = int(os.environ.get('CACHE_REDIS_PORT', 6379))
+    CACHE_REDIS_DB = int(os.environ.get('CACHE_REDIS_DB', 0))
     CACHE_REDIS_URL = os.environ.get('CACHE_REDIS_URL')
-    CACHE_DEFAULT_TIMEOUT = os.environ.get('CACHE_DEFAULT_TIMEOUT')
+    CACHE_DEFAULT_TIMEOUT = int(os.environ.get('CACHE_DEFAULT_TIMEOUT', 300))
